b3e191b6dbaf52f05a30afd822e6ed084fdf4dc6
heliton Vieira <helitondba@gmail.com>
2019-09-06 09:40:32 -0300

    Projeto foi criado

---
+++ src/main/java/com/helitonvieira/helisistema/HelisistemaApplication.java
@@ -0,0 +1,13 @@
+package com.helitonvieira.helisistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelisistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelisistemaApplication.class, args);
+	}
+
+}
