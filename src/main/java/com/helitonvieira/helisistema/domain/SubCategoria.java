290d4a5da5bca55e64c4edf6df1554bcf014001b
heliton vieira <helitondba@gmail.com>
2019-09-06 12:57:21 -0300

    add base teste h2

--- src/main/java/com/helitonvieira/helisistema/domain/SubCategoria.java
+++ src/main/java/com/helitonvieira/helisistema/domain/SubCategoria.java
@@ -2,9 +2,17 @@ package com.helitonvieira.helisistema.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class SubCategoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+
 private Integer cod_subcategoria;
 private String des_subcategoria;
 
