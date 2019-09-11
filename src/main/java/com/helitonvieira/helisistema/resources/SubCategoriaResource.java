bb06256687f31b3dea04edeb81b76ff45b353d98
heliton vieira <helitondba@gmail.com>
2019-09-11 14:28:40 -0300

    inserindo nova subcategoria com post

--- src/main/java/com/helitonvieira/helisistema/resources/SubCategoriaResource.java
+++ src/main/java/com/helitonvieira/helisistema/resources/SubCategoriaResource.java
@@ -1,14 +1,18 @@
 package com.helitonvieira.helisistema.resources;
 
+import java.net.URI;
+
 //import java.util.ArrayList;
 //import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.helitonvieira.helisistema.domain.SubCategoria;
 import com.helitonvieira.helisistema.services.SubCategoriaService;
@@ -24,5 +28,13 @@ public class SubCategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		SubCategoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody SubCategoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getCod_subcategoria()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/helitonvieira/helisistema/services/SubCategoriaService.java
+++ src/main/java/com/helitonvieira/helisistema/services/SubCategoriaService.java
@@ -9,17 +9,20 @@ import com.helitonvieira.helisistema.domain.SubCategoria;
 import com.helitonvieira.helisistema.repositories.SubCategoriaRepository;
 import com.helitonvieira.helisistema.services.exceptions.ObjectNotFoundException;
 
-
-
 @Service
 public class SubCategoriaService {
 
 	@Autowired
 	private SubCategoriaRepository repo;
 
-	public SubCategoria buscar(Integer id)  {
-	 Optional<SubCategoria> obj = repo.findById(id);
+	public SubCategoria buscar(Integer id) {
+		Optional<SubCategoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-			 "Objeto não encontrado Codigo: " + id + ", Tipo: = " + SubCategoria.class.getName()));
+				"Objeto não encontrado Codigo: " + id + ", Tipo: = " + SubCategoria.class.getName()));
+	}
+	
+	public SubCategoria insert(SubCategoria obj){
+		obj.setCod_subcategoria(null);
+		return repo.save(obj);
 	}
 }
