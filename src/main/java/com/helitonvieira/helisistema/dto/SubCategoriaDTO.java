871f18dd416736a2f9fa03d9c05202ab76dc1876
heliton vieira <helitondba@gmail.com>
2019-09-12 13:47:00 -0300

    aula 36 personalizando retorno da validacao

--- src/main/java/com/helitonvieira/helisistema/dto/SubCategoriaDTO.java
+++ src/main/java/com/helitonvieira/helisistema/dto/SubCategoriaDTO.java
@@ -2,46 +2,45 @@ package com.helitonvieira.helisistema.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.NotEmpty;
 
 import com.helitonvieira.helisistema.domain.SubCategoria;
 
-public class SubCategoriaDTO implements Serializable{
+public class SubCategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
 
-private Integer cod_subcategoria;
+	private Integer cod_subcategoria;
 
-@NotEmpty(message="Preenchimento obrigatório")
-@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
-private String des_subcategoria;
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 3, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
+	private String des_subcategoria;
 
-public SubCategoriaDTO() {	
-}
+	public SubCategoriaDTO() {  
+	}
 
-public SubCategoriaDTO(SubCategoria obj) {
-	cod_subcategoria = obj.getCod_subcategoria();
-	
-	des_subcategoria = obj.getDes_subcategoria();
-	
-}
+	public SubCategoriaDTO(SubCategoria obj) {
+		cod_subcategoria = obj.getCod_subcategoria();
 
-public Integer getCod_subcategoria() {
-	return cod_subcategoria;
-}
+		des_subcategoria = obj.getDes_subcategoria();
 
-public void setCod_subcategoria(Integer cod_subcategoria) {
-	this.cod_subcategoria = cod_subcategoria;
-}
+	}
 
-public String getDes_subcategoria() {
-	return des_subcategoria;
-}
+	public Integer getCod_subcategoria() {
+		return cod_subcategoria;
+	}
 
-public void setDes_subcategoria(String des_subcategoria) {
-	this.des_subcategoria = des_subcategoria;
-}
+	public void setCod_subcategoria(Integer cod_subcategoria) {
+		this.cod_subcategoria = cod_subcategoria;
+	}
 
+	public String getDes_subcategoria() {
+		return des_subcategoria;
+	}
+
+	public void setDes_subcategoria(String des_subcategoria) {
+		this.des_subcategoria = des_subcategoria;
+	}
 
 }
--- src/main/java/com/helitonvieira/helisistema/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/helitonvieira/helisistema/resources/exception/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,5 +28,52 @@ public class ResourceExceptionHandler {
 		StandarError err = new StandarError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandarError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(),"Erro de Validação",System.currentTimeMillis());
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	/*@ExceptionHandler(AuthorizationException.class)
+	public ResponseEntity<StandardError> authorization(AuthorizationException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.FORBIDDEN.value(), "Acesso negado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+	}
+
+	@ExceptionHandler(FileException.class)
+	public ResponseEntity<StandardError> file(FileException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Erro de arquivo", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonService(AmazonServiceException e, HttpServletRequest request) {
+		
+		HttpStatus code = HttpStatus.valueOf(e.getErrorCode());
+		StandardError err = new StandardError(System.currentTimeMillis(), code.value(), "Erro Amazon Service", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(code).body(err);
+	}
+
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardError> amazonClient(AmazonClientException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Erro Amazon Client", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(AmazonS3Exception.class)
+	public ResponseEntity<StandardError> amazonS3(AmazonS3Exception e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Erro S3", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}*/
+
 	
 }
